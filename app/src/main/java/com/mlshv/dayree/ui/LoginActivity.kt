dabf62f554544e3da0b85330ff4bbc0065f195c2
Mikhail Malyshev <mlshv42@yandex.ru>
2016-11-12 01:48:22 +0300

    Add ui package

---
+++ app/src/main/java/com/mlshv/dayree/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mlshv.dayree.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mlshv.dayree.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
