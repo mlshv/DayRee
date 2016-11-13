56151c15203621e6592a4841ee72f4f0ee347400
Mikhail Malyshev <mlshv42@yandex.ru>
2016-11-13 19:18:33 +0300

    Add set password/login feature

--- app/src/main/java/com/mlshv/dayree/DayReeApplication.kt
+++ app/src/main/java/com/mlshv/dayree/DayReeApplication.kt
@@ -1,5 +1,31 @@
 package com.mlshv.dayree
 
 import android.app.Application
+import com.mlshv.dayree.db.DatabaseHelper
+import net.sqlcipher.database.SQLiteDatabase
 
-class DayReeApplication : Application()
+class DayReeApplication : Application() {
+    companion object {
+        private var singleton: DayReeApplication? = null
+        fun getInstance(): DayReeApplication {
+            return singleton!!
+        }
+    }
+
+    private val dbHelper = DatabaseHelper(this)
+
+    override fun onCreate() {
+        super.onCreate()
+        singleton = this
+        // SQLCipher initialization
+        SQLiteDatabase.loadLibs(this)
+    }
+
+    fun isDatabaseExists() = dbHelper.isDatabaseExists()
+
+    fun isPasswordCorrect(password: String) = dbHelper.isDatabasePasswordCorrect(password)
+
+    fun createDatabaseWithPassword(password: String) {
+        val db = dbHelper.getWritableDatabase(password)
+    }
+}
--- app/src/main/java/com/mlshv/dayree/ui/LoginActivity.kt
+++ app/src/main/java/com/mlshv/dayree/ui/LoginActivity.kt
@@ -1,33 +1,59 @@
 package com.mlshv.dayree.ui
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.mlshv.dayree.R
 import android.content.Intent
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
-
+import com.mlshv.dayree.DayReeApplication
 
 
 class LoginActivity : AppCompatActivity() {
-
+    val app: DayReeApplication = DayReeApplication.getInstance()
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val passwordEditText = findViewById(R.id.password_text) as EditText
+        val repeatPasswordEditText = findViewById(R.id.repeat_password_text) as EditText
         val loginButton = findViewById(R.id.login_button) as Button
-        val passwordTextView = findViewById(R.id.password_text) as EditText
+
+        if (app.isDatabaseExists()) {
+            loginButton.setText(R.string.log_in)
+            (repeatPasswordEditText.parent as RelativeLayout).removeView(repeatPasswordEditText)
+            val layoutParams = RelativeLayout.LayoutParams(loginButton.layoutParams)
+            layoutParams.addRule(RelativeLayout.BELOW, R.id.password_text)
+            loginButton.layoutParams = layoutParams
+        }
 
         loginButton.setOnClickListener {
-            if (passwordTextView.text.toString() == "hardcoded") {
-                val myIntent = Intent(this, MainActivity::class.java)
-                this.startActivity(myIntent)
+            val password = passwordEditText.text.toString()
+            val passwordsMatch = password == repeatPasswordEditText.text.toString()
+
+            if (app.isDatabaseExists()) {
+                if (app.isPasswordCorrect(password)) {
+                    goToMainActivity()
+                } else {
+                    Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show()
+                }
             } else {
-                val toast = Toast.makeText(applicationContext,
-                        passwordTextView.text, Toast.LENGTH_SHORT)
-                toast.show()
+                if (passwordsMatch) {
+                    app.createDatabaseWithPassword(password)
+                    goToMainActivity()
+                } else {
+                    Toast.makeText(this, "Passwords don't match", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+
+    fun goToMainActivity() {
+        val myIntent = Intent(this, MainActivity::class.java)
+        this.startActivity(myIntent)
+        this.finish()
     }
 }
