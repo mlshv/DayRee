2f06382bd5056b89be40212d8f20d58c93b96c18
Mikhail Malyshev <mlshv42@yandex.ru>
2016-12-04 02:12:41 +0300

    Make tabs scrollable

--- app/src/main/java/com/mlshv/dayree/ui/MainActivity.kt
+++ app/src/main/java/com/mlshv/dayree/ui/MainActivity.kt
@@ -6,39 +6,45 @@ import android.support.v7.app.AppCompatActivity
 import com.mlshv.dayree.R
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter
-import android.widget.Toast
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationViewPager
 
 
 class MainActivity : AppCompatActivity() {
+
+    var viewPager : AHBottomNavigationViewPager? = null
+    var bottomNavigation : AHBottomNavigation? = null
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initBottomNavigation()
 
-        val viewPager = findViewById(R.id.main_pager) as ViewPager
+        viewPager = findViewById(R.id.main_pager) as AHBottomNavigationViewPager
+        viewPager!!.setPagingEnabled(true)
         val pagerAdapter = MainPagerAdapter(supportFragmentManager)
-        viewPager.adapter = pagerAdapter
+        viewPager!!.adapter = pagerAdapter
+        viewPager!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                bottomNavigation!!.currentItem = position
+            }
+        })
     }
 
     private fun initBottomNavigation() {
         val tabColors = applicationContext.resources.getIntArray(R.array.tab_colors)
-        val bottomNavigation = findViewById(R.id.bottom_navigation) as AHBottomNavigation
-        bottomNavigation.isColored = true
+        bottomNavigation = findViewById(R.id.bottom_navigation) as AHBottomNavigation
+        bottomNavigation!!.isColored = true
         val navigationAdapter = AHBottomNavigationAdapter(this, R.menu.bottom_navigation_menu)
         navigationAdapter.setupWithBottomNavigation(bottomNavigation, tabColors)
         
-        bottomNavigation.setOnTabSelectedListener { tabPosition, wasSelected ->
-            when(tabPosition) {
-                0 -> {
-                    Toast.makeText(this, "Tab 1", Toast.LENGTH_SHORT).show()
-                }
-                1 -> {
-                    Toast.makeText(this, "Tab 2", Toast.LENGTH_SHORT).show()
-                }
-                2 -> {
-                    Toast.makeText(this, "Tab 3", Toast.LENGTH_SHORT).show()
-                }
-            }
+        bottomNavigation!!.setOnTabSelectedListener { tabPosition, wasSelected ->
+            viewPager!!.currentItem = tabPosition
             true
         }
     }
