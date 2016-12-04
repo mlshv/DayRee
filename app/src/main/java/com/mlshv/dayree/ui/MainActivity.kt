dc98f59033fc17dc35414b923fc737dbb0e7ce13
Mikhail Malyshev <mlshv42@yandex.ru>
2016-12-04 18:53:49 +0300

    Add Floating Action Button

--- app/src/main/java/com/mlshv/dayree/ui/MainActivity.kt
+++ app/src/main/java/com/mlshv/dayree/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.mlshv.dayree.ui
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import com.mlshv.dayree.R
@@ -9,31 +12,20 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationViewPager
 
 
+
+
 class MainActivity : AppCompatActivity() {
 
     var viewPager : AHBottomNavigationViewPager? = null
+    var floatingActionButton : FloatingActionButton? = null
     var bottomNavigation : AHBottomNavigation? = null
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initBottomNavigation()
-
-        viewPager = findViewById(R.id.main_pager) as AHBottomNavigationViewPager
-        viewPager!!.setPagingEnabled(true)
-        val pagerAdapter = MainPagerAdapter(supportFragmentManager)
-        viewPager!!.adapter = pagerAdapter
-        viewPager!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-            }
-            override fun onPageSelected(position: Int) {
-                bottomNavigation!!.currentItem = position
-            }
-        })
+        initViewPager()
+        initFloatingActionButton()
     }
 
     private fun initBottomNavigation() {
@@ -46,6 +38,47 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation!!.setOnTabSelectedListener { tabPosition, wasSelected ->
             viewPager!!.currentItem = tabPosition
             true
+        }
+    }
+
+    private fun initViewPager() {
+        viewPager = findViewById(R.id.main_pager) as AHBottomNavigationViewPager
+        viewPager!!.setPagingEnabled(true)
+        val pagerAdapter = MainPagerAdapter(supportFragmentManager)
+        viewPager!!.adapter = pagerAdapter
+        viewPager!!.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                bottomNavigation!!.currentItem = position
+                reshowFab()
+            }
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+        })
+    }
+
+    private fun initFloatingActionButton() {
+        floatingActionButton = findViewById(R.id.fab) as FloatingActionButton
+    }
+
+    private fun reshowFab() {
+        floatingActionButton?.hide(fabChangeListener)
+    }
+
+    val fabChangeListener = object : FloatingActionButton.OnVisibilityChangedListener() {
+        private fun getColorForCurrentTabPosition() : Int {
+            val tabPosition = viewPager!!.currentItem
+            when(tabPosition) {
+                0 -> return R.color.color_tab_0
+                1 -> return R.color.color_tab_1
+                2 -> return R.color.color_tab_2
+            }
+            null!!
+        }
+
+        override fun onHidden(fab: FloatingActionButton?) {
+            super.onHidden(fab)
+            fab!!.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this@MainActivity, getColorForCurrentTabPosition()))
+            fab.show()
         }
     }
 }
